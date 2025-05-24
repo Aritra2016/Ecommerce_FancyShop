43e7fd9309e4dcca34e7dbd9b50a30782c9c22e0
Aritra Khatua <aritrakhatua20@gmail.com>
2025-05-24 09:17:35 +0530

    commit_01

---
+++ src/test/java/org/aritra/ecommerce_fancyshop/EcommerceFancyShopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aritra.ecommerce_fancyshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceFancyShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
